71eb96942091c87f424d2d37d5054501c283caf2
Aura Husnaini P.Z <78643049+auraahsn@users.noreply.github.com>
2023-01-23 13:37:50 +0700

    v_struktur

---
+++ application/views/user/v_strukturOrganisasi.php
@@ -0,0 +1,18 @@
+<div class="features">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <center>
+                    <br><br><b><br></b>
+                    <h2 class="section_title">Struktur Organisasi</h2><br>
+                </center>
+                <div class="card" style="width: 100%;">
+                    <img class="card-img-top" src="<?= base_url('template\img\struktur.jpg') ?>" alt="struktur_organisasi">
+                    <div class="card-body">
+                        <p class="card-text">Struktur Organisasi PT Jasa Raharja Putera Cabang Bandar Lampung</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
