0f6ae10357064d93213cc39c3ae05d4b7c6fc3de
Aura Husnaini P.Z <78643049+auraahsn@users.noreply.github.com>
2023-01-29 17:37:38 +0700

    rev_nav2

--- application/views/layout/v_nav.php
+++ application/views/layout/v_nav.php
@@ -4,44 +4,56 @@
 				<div class="row">
 					<div class="col">
 						<div class="header_content d-flex flex-row align-items-center justify-content-start">
-							<div class="logo_container">
-								<a href="<?= base_url('User') ?>">
-									<img src="<?= base_url('template\img\logo_jp_new.jpg')?>" alt="logo_jp" height="90px" width="90px">
-									<div class="logo_text">JP<span> Lampung</span></div>
-								</a>
-							</div>
-							<nav class="main_nav_contaner ml-auto">
-								<ul class="main_nav">
-									<li class="active"><a href="<?= base_url('User') ?>">Home</a></li>
-
-									<li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Tentang Kami
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Sejarah</a>
-          <a class="dropdown-item" href="#">Visi & misi</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Profil</a>
-          <a class="dropdown-item" href="#">Profil Management</a>
-          <a class="dropdown-item" href="#">Struktur Organisasi</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Penghargaan Perusahaan </a>
-        </div>
-      </li>
-									<li><a href="produk.html">Produk</a></li>
-									<li><a href="jrpsyariah.html">Layanan</a></li>
-									<li><a href="jrpsyariah.html">JRP Syariah</a></li>
-								</ul>
-								<div class="search_button"><i class="fa fa-search" aria-hidden="true"></i></div>
-
-								<!-- Hamburger -->
-
-								<!-- <div class="shopping_cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></div> -->
-								<div class="hamburger menu_mm">
-									<i class="fa fa-bars menu_mm" aria-hidden="true"></i>
+							<a href="<?= base_url('User') ?>"><img width="130px" height="90px" src="https://jrp.co.id/wp-content/uploads/2019/08/output-onlinegiftools.gif" class="attachment-post-thumbnail size-post-thumbnail" alt="" loading="lazy">
+								<div class="logo_container ml-3">
+									<a href="<?= base_url('User') ?>">
+										<img src="<?= base_url('template\img\logo_jp.png') ?>" alt="logo_jp" height="100px" width="100px">
+										<!-- <div class="logo_text">Jasa <span> Raharja Putera</span></div>
+									<p>Branch Office Bandar Lampung</p> -->
+									</a>
 								</div>
-							</nav>
+								<nav class="main_nav_contaner ml-auto">
+									<ul class="main_nav">
+										<li class="active"><a href="<?= base_url('User') ?>">Home</a></li>
+
+										<li class="nav-item dropdown">
+											<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+												About Us
+											</a>
+											<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+												<a class="dropdown-item" href="<?= base_url('User/sejarah') ?>">Sejarah</a>
+												<a class="dropdown-item" href="<?= base_url('User/visiMisi') ?>">Visi & misi</a>
+												<div class="dropdown-divider"></div>
+												<a class="dropdown-item" href="#">Profil</a>
+												<a class="dropdown-item" href="#">Profil Management</a>
+												<a class="dropdown-item" href="<?= base_url('user/strukturOrganisasi') ?>">Struktur Organisasi</a>
+												<div class="dropdown-divider"></div>
+												<a class="dropdown-item" href="<?= base_url('User/penghargaan') ?>">Penghargaan Perusahaan </a>
+											</div>
+										</li>
+										<li class="nav-item dropdown">
+											<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+												Product
+											</a>
+											<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+												<a class="dropdown-item" href="<?= base_url('User/astor') ?>">JP-ASTOR</a>
+												<a class="dropdown-item" href="<?= base_url('User/aspri') ?>">JP-ASPRI</a>
+												<a class="dropdown-item" href="<?= base_url('User/bonding') ?>">JP-BONDING</a>
+												<a class="dropdown-item" href="<?= base_url('User/graha') ?>">JP-GRAHA</a>
+											</div>
+										</li>
+										<li><a href="#footer">Contact</a></li>
+										<!-- <li><a href="jrpsyariah.html">JRP Syariah</a></li> -->
+									</ul>
+									<!-- <div class="search_button"><i class="fa fa-search" aria-hidden="true"></i></div> -->
+
+									<!-- Hamburger -->
+
+									<!-- <div class="shopping_cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i></div> -->
+									<div class="hamburger menu_mm">
+										<i class="fa fa-bars menu_mm" aria-hidden="true"></i>
+									</div>
+								</nav>
 
 						</div>
 					</div>
