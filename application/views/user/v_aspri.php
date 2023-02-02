8ae48395cf58066d61d0ab08fe1bed5ead2e2d49
Aura Husnaini P.Z <78643049+auraahsn@users.noreply.github.com>
2023-02-02 10:46:10 +0700

    jp aspri

---
+++ application/views/user/v_aspri.php
@@ -0,0 +1,206 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="main_styles.css"> -->
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>template/front-end/styles/about.css">
+
+    <!-- <title>Hello, world!</title> -->
+</head>
+
+<body>
+    <div class="features">
+        <div class="container">
+            <center>
+                <br><br><br>
+                <h1 class="section_title">JP-ASPRI</h1>
+                <h2 class="section_title">Asuransi Kecelakaan Diri</h2>
+                <hr class="style5" style="border-top: 5px solid #08347c" width="100px">
+            </center>
+            <hr class="style5" style="border-top: 1px solid #08347c centered" width="100px">
+            <br>
+            <div class="row">
+                <div class="col-5">
+                    <br>
+                    <div class="section_title_container text-justify">
+                        <div class="section_subtitle">
+                            <div class="card" style="width: 27rem;">
+                                <img src="<?= base_url('template\img\jp_aspri.png') ?>" class="card-img-top" alt="...">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <br><br>
+                    <div class="card border mb-3 shadow-sm p-3 mb-5 bg-white rounded" style="max-width: 100%;">
+                        <div class="card-body">
+                            <div class="card-body text-justify">
+                                <p class="card-text fs-6">Kecelakaan, dapat terjadi dimana saja, dan bersifat tiba-tiba. Tidak hanya di jalan raya tapi juga di rumah, di tempat kerja, pusat keramaian atau banyak tempat lainnya. Anda tidak akan pernah menduga apa yang akan terjadi.</p>
+                                <p class="fs-6">JP-ASPRI adalah solusi yang tepat untuk memberikan perlindungan bagi Anda, orang-orang yang Anda sayangi, karyawan Anda ataupun rekan-rekan Anda dari risiko kecelakaan, karena JP-ASPRI tidak hanya melindungi di tempat kerja atau di sekolah, tetapi selama 24 jam dimana saja berada. Jangan menunggu waktu percayakan sepenuhnya perlindungan asuransi kecelakaan pribadi pada JP-ASPRI, produk asuransi terpercaya dari JRP-INSURANCE.</p><br>
+                                <div class="d-grid gap-2 d-md-block">
+                                    <button class="btn btn-primary fs-6" type="button">Daftar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <center>
+                    <hr class="style5" style="border-top: 5px solid #08347c" width="100px">
+                    <br>
+                    <h4 class="section_title">DENGAN JP-ASPRI ANDA DAPAT MEMBERIKA PERLINDUNGAN 24 JAM NON-STOP</h4>
+                    <h4 class="section_title">BAGI KELUARGA ANDA, REKAN-REKAN ANDA DAN KARYAWAN ANDA</h4>
+                    <h4 class="section_title">DARI RESIKO KECELAKAAN YANG DAPAT TERJADI KAPAN DAN DIMANAPUN BERADA.
+                </center>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="team_item">
+                            <div class="team_image"><img src="<?= base_url('template\img\pom.png') ?>" alt="pom" width="100px" class="mt-5 ml-5"></div>
+                            <div class="team_body">
+                                <div class="team_title"><a href="#">KETENANGAN PIKIRAN</a></div>
+                                <div class="team_subtitle">Jalani tujuanmu tanpa harus khawatir, kami bersamamu.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="team_item">
+                            <div class="team_image"><img src="<?= base_url('template\img\dream.png') ?>" alt="pom" width="100px" class="mt-5 ml-5"></div>
+                            <div class="team_body">
+                                <div class="team_title"><a href="#">TETAPKAN JALAN MIMPIMU</a></div>
+                                <div class="team_subtitle">Wujudkan cita - citamu serta gapai mimpimu dengan keyakinan dengan pelayanan kami.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="team_item">
+                            <div class="team_image"><img src="<?= base_url('template\img\diamond.png') ?>" alt="pom" width="100px" class="mt-5 ml-5"></div>
+                            <div class="team_body">
+                                <div class="team_title"><a href="#">CAKUPAN YANG LUAS</a></div>
+                                <div class="team_subtitle">Dapatkan keuntungan dalam cakupan yang luas dalam penanganan kesehatan anda dan keluarga.</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <!-- Kelengkapan Berkas -->
+                <div class="container">
+                    <br><br><br>
+                    <center>
+                        <hr class="style5" style="border-top: 5px solid #08347c" width="100px">
+                    </center>
+                    <br>
+                    <div class="section_title_container text-center">
+                        <h2 class="section_title">Kelengkapan Berkas Klaim</h2><br>
+                    </div>
+                    <div class="row text-justify">
+                        <div class="col">
+                            <div class="cardShadow">
+                                <div class="card shadow p-3 mb-2 bg-white rounded">
+                                    <div class="card-body">
+                                        <p class="card-text">
+                                        <ul style="list-style-type:circle">
+                                            <li class="fs-6">Tertanggung sesegera mungkin melaporkan/menyampaikan keterangan tertulis mengenai kerugian yang terjadi dengan tidak merubah/merusak objek yang mengalami kerugian.</li>
+                                            <li class="fs-6">Pengajuan klaim dilakukan dengan:
+                                                - Mengisi formulir Laporan Klaim Asuransi Kecelakaan Diri (LK1) <br>
+                                                - Melampirkan kwitansi biaya pengobatan <br>
+                                                - Melampirkan surat kematian, copy Kartu Keluarga dan KTP khusus untuk meninggal dunia <br>
+                                                - Untuk korban cacat tetap dilengkapi dengan laporan kesehatan terakhir dari dokter. <br>
+                                                - Dokumen pendukung lainnya yang diperlukan <br>
+                                            </li>
+                                        </ul>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><br><br><br>
+
+                <!-- Flip Card -->
+                <div class="row">
+                    <div class="col">
+                        <div class="flip-card" style="width: 500px;">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front" style="background-image: url('https://s25.postimg.cc/frbd9towf/cta-2.png');">
+                                    <img src="<?= base_url('template\img\shield.png') ?>" alt="shield" style="width: 80px; height:80px" class="mb-3">
+                                    <h3>KELEBIHAN JP-ASPRI</h3>
+                                    <!-- <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;"> -->
+                                </div>
+                                <div class="flip-card-back">
+                                    <ul style="list-style-type:circle" class="p-5">
+                                        <li>Proses PERMOHONAN SANTUNAN SEDERHANA </li>
+                                        <li>Mendapat JAMINAN GANDA, bila kecelakaan yang terjadi juga dalam ruang lingkup Jaminan kecelakaan Lalu Lintas Jalan sesuai UU No. 33 & 34 Tahun 1964.</li>
+                                        <li>SANTUNAN DITERIMA LEBIH DARU SATU KALI, selama kartu peserta masih berlaku</li>
+                                        <li>Identitas KARTU PESERTA ASURANSI EKSKLUSIF</li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div><br><br>
+                    <div class="col">
+                        <div class="flip-card" style="width: 500px;">
+                            <div class="flip-card-inner">
+                                <div class="flip-card-front" style="background-image: url('https://s25.postimg.cc/frbd9towf/cta-2.png');">
+                                    <img src="<?= base_url('template\img\info.png') ?>" alt="shield" style="width: 80px; height:80px" class="mb-3">
+                                    <h3>KECELAKAAN YANG TIDAK DIJAMIN</h3>
+                                    <!-- <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;"> -->
+                                </div>
+                                <div class="flip-card-back">
+                                    <ul style="list-style-type:circle" class="p-4">
+                                        <li>Tertanggung turut serta dalam kegiatan olahraga berbahaya seperti tinju, Gulat, Karate, Judo, Arung Jeram, Diving, Snorkeling, Terjun Payung, Mendaki Gunung, Berburu, turut serta dalam perlombaan/ketangkasan. </li>
+                                        <li>Tertanggung dengan sengaja melakukan kejahatan atau turut serta dalam kejahatan, huruhara, penggunaan obat terlarang, mabok,bunuh diri.</li>
+                                        <li>Tertanggung bertindak sebagai pengemudi kendaraan bermotor tanpa memiliki SIM (surat izin mengemudi) yang sah.</li>
+                                        <li>Mengikuti Latihan Militer.</li>
+                                        <li>Menggunakan obat-obatan terlarang, Narkotika, dan Obat sejenis yang dapat mengakibatkan kematian</li>
+                                        <li>Penyakit Covid19</li>
+                                    </ul>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div><br><br>
+                    <!-- <div class="container">
+                    <div class="row">
+                        <div class="box-item col-md-4">
+                            <div class="flip-box">
+                                <div class="inner color-white">
+                                    <div class="flip-box-front text-center" style="background-image: url('https://s25.postimg.cc/frbd9towf/cta-2.png');">
+                                        <h3 class="flip-box-header">KELEBIHAN</h3>
+                                        <p>A short sentence describing this callout</p>
+                                        <img src="https://s25.postimg.cc/frbd9towf/cta-2.png" alt="" class="flip-box-img">
+                                    </div>
+                                    <div class="flip-box-back text-center" style="background-image: url('https://s25.postimg.cc/frbd9towf/cta-2.png');">
+                                        <h3 class="flip-box-header">KELEBIHAN</h3>
+                                        <p>A short sentence describing this callout</p>
+                                        <button class="flip-box-button">Learn More</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> -->
+                </div><br><br>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
