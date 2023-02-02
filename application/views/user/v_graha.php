d9997030c679686ccf4696e2c01c7580557b0127
Aura Husnaini P.Z <78643049+auraahsn@users.noreply.github.com>
2023-02-02 10:48:33 +0700

    view

---
+++ application/views/user/v_graha.php
@@ -0,0 +1,210 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="main_styles.css"> -->
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>template/front-end/styles/about.css">
+
+    <!-- <title>Hello, world!</title> -->
+</head>
+
+<body>
+    <div class="features">
+        <div class="container">
+            <center>
+                <br><br><br>
+                <h1 class="section_title">JP-GRAHA</h1>
+                <h2 class="section_title">Asuransi Kebakaran (Properti)</h2>
+                <hr class="style5" style="border-top: 5px solid #08347c" width="100px">
+            </center>
+            <br>
+            <div class="row">
+                <div class="col-5">
+                    <br>
+                    <div class="section_title_container text-justify">
+                        <div class="section_subtitle">
+                            <div class="card" style="width: 27rem;">
+                                <img src="<?= base_url('template\img\jp_graha.png') ?>" class="card-img-top" alt="...">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <br><br>
+                    <div class="card border mb-3 shadow-sm p-3 mb-5 bg-white rounded" style="max-width: 100%;">
+                        <div class="card-body">
+                            <div class="card-body text-justify">
+                                <p class="card-text fs-6">Kami yakin bahwa Anda tidak ingin kehilangan asset berharga karena musibah yang dapat terjadi sewaktu-waktu. Untuk itu lindungi harta dan bangunan Anda dengan JP-GRAHA. </p>
+                                <p class="fs-6">JP-GRAHA adalah asuransi yang tepat untuk melindungi asset bangunan Anda dan harta benda didalamnya dari kerugian dan kerusakan akibat kebakaran atau peristiwa lain yang dijamin oleh pollis dan perluasannya. Apapun asset Anda baik itu rumah tinggal, tempat usaha, kantor maupun pabrik, besar maupun kecil. Jangan menunggu waktu, percayakan sepenuhnya pada JP-GRAHA, produk terpercaya dari JRP-INSURANCE.</p><br>
+                                <div class="d-grid gap-2 d-md-block">
+                                    <button class="btn btn-primary fs-6" type="button">Daftar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-6  text-justify">
+                        <br><br>
+                        <div class="card border mb-3 shadow-sm p-3 mb-5 bg-white rounded" style="max-width: 100%;">
+                            <div class="card-header">
+                                <h2><b>Ruang Lingkup JP-GRAHA</b></h2>
+                            </div>
+                            <div class="card-body fs-6 text-dark">
+                                Anda dapat leluasa memilih pembayaran premi sesuai keinginan, mulai dari yang Standar sampai dengan tambahan perluasan jaminan yang anda kehendaki, seperti Asuransi Kebakaran dengan Jaminan Standar.
+                                Dengan jaminan ini, bangunan serta harta benda didalamnya dan atau kepentingan lainnya akan terjamin dari risiko keuangan yang disebabkan kebakaran, sambaran petir, ledakan, kejatuhan pesawat terbang, dan akibat asap.
+                            </div>
+                            <div class="card-body">
+                                <div class="card-body">
+                                    <h4 class="card-title">BANGUNAN YANG DAPAT DIASURANSIKAN</h4>
+                                    <p class=" fs-6">JP-GRAHA melindungi seluruh asset Anda baik itu bangunan rumah tinggal, kantor, gudang, hotel, sekolah, rumah sakit, apartemen, toko, pabrik dan bangunan lainnya.</p>
+                                </div>
+                                <div class="card-body">
+                                    <h4 class="card-title">HARTA BENDA YANG DAPAT DIASURANSIKAN</h4>
+                                    <p class=" fs-6">JP-GRAHA tidak hanya melindungi bangunan Anda tapi juga harta benda didalamnya seperti Perabot, Mesin, Stok dan isinya.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <br>
+                        <div class="section_title_container text-justify">
+                            <div class="section_subtitle">
+                                <div class="card" style="width: 30rem;">
+                                    <img src="<?= base_url('template\img\graha.png') ?>" class="card-img-top" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Accordion (Jaminan Tambahan) -->
+            <hr class="style5" style="border-top: 2px solid #08347c">
+            <br>
+            <div class="feature">
+                <div class="feature_background" style="background-image:url(images/courses_background.jpg)"></div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="section_title_container text-center">
+                                <h2 class="section_title">JAMINAN TAMBAHAN</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row feature_row">
+
+                        <!-- Feature Content -->
+                        <div class="col-lg-15 feature_col">
+                            <div class="feature_content">
+                                <!-- Accordions -->
+                                <div class="accordions  text-justify">
+                                    <div class="elements_accordions">
+                                        <div class="accordion_container">
+                                            <div class="accordion d-flex flex-row align-items-center active">
+                                                <div>Jaminan Resiko Banjir</div>
+                                            </div>
+                                            <div class="accordion_panel">
+                                                <p>Bangunan dan Harta Benda serta kepentingan yang dipertanggungkan akan terjamin dari kerusakan yang diakibatkan langsung oleh terjadinya banjir</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="accordion_container">
+                                            <div class="accordion d-flex flex-row align-items-center">
+                                                <div>Jaminan Resiko Terosis dan Sabotase</div>
+                                            </div>
+                                            <div class="accordion_panel">
+                                                <p>Bangunan dan Harta benda serta kepentingan yang dipertanggungan akan terjamin dari kerusakan dan kerugian financial yang disebabkan oleh salah satu atau lebih dari risiko-risiko akibat terorisme, sabotase dan atau penjarahan selama berlangsungnya terorisme dan sabotase</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="accordion_container">
+                                            <div class="accordion d-flex flex-row align-items-center">
+                                                <div>Jaminan Resiko Gempa Bumi</div>
+                                            </div>
+                                            <div class="accordion_panel">
+                                                <p>Bangunan dan Harta benda serta kepentingan yang dipertanggungkan akan terjamin dari risiko-risiko kerugian financial yang secara langsung diakibatkan oleh Gempa Bumi, Letusan Gunung Berapi, Kebakaran & Ledakan yang mengikuti terjadinya gempa bumi dan atau letusan gunung berapi serta Tsunami.</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="accordion_container">
+                                            <div class="accordion d-flex flex-row align-items-center">
+                                                <div>Jaminan Resiko Kebongkaran</div>
+                                            </div>
+                                            <div class="accordion_panel">
+                                                <p>Bangunan dan harta benda serta kepentingan yang dipertanggungkan akan terjamin dari risiko-risiko kerugian yang dilakukan dengan pembongkaran atau pencurian yang secara langsung diakibatkan pencurian financial dengan kekerasan / perampokan.</p>
+                                            </div>
+                                        </div>
+
+                                        <div class="accordion_container">
+                                            <div class="accordion d-flex flex-row align-items-center">
+                                                <div>Property/Industry All Risk (PAR/IAR)</div>
+                                            </div>
+                                            <div class="accordion_panel">
+                                                <p>Dengan polis ini, bangunan dan harta benda serta kepentingan yang dipertanggungkan akan terjamin dari risiko-risiko seperti kebakaran, ledakan, banjir, kerusuhan, huru-hara, kebongkaran dan risiko lainnya yang tidak dikecualikan dalam polis.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Accordions End -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Kelengkapan Berkas -->
+            <div class="container">
+                <center>
+                    <hr class="style5" style="border-top: 5px solid #08347c" width="100px">
+                </center>
+                <br>
+                <div class="section_title_container text-center">
+                    <h2 class="section_title">Kelengkapan Berkas Klaim</h2><br>
+                </div>
+                <div class="row text-justify">
+                    <div class="col">
+                        <div class="cardShadow">
+                            <div class="card shadow p-3 mb-2 bg-white rounded">
+                                <div class="card-body">
+                                    <p class="card-text">
+                                    <ul style="list-style-type:circle">
+                                        <li class="fs-6">Tertanggung sesegera mungkin melaporkan/menyampaikan keterangan tertulis (sesuai PSKI) mengenai kerugian yang terjadi dengan tidak merubah/merusak objek yang mengalami kerugian.</li>
+                                        <li class="fs-6">Pengajuan Klaim dilakukan dengan mengisi Formulir Klaim atau keterangan tertulis (surat/faksimile) dengan melampirkan :
+                                            - Copy Polis <br>
+                                            - Surat tuntutan kerugia <br>
+                                            - Surat keterangan kejadia <br>
+                                            - Estimasi kerugian <br>
+                                            - Surat keterangan dari kepolisia <br>
+                                            - Dokumen pendukung lainnya yang diperlukan
+                                        </li>
+                                    </ul>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div><br><br><br>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
